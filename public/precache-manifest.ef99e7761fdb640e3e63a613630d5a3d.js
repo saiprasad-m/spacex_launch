80f05809158f3c0e6b4e5386ac84ae04e47b3935
Sai Prasad M <sai.prasad@rocketmail.com>
2018-12-26 21:27:01 +0530

    Added pagination for launches

---
+++ public/precache-manifest.ef99e7761fdb640e3e63a613630d5a3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "38d6b9ccf3f75229c611",
+    "url": "/static/js/main.38d6b9cc.chunk.js"
+  },
+  {
+    "revision": "1da9aef565efaccc8d26",
+    "url": "/static/js/1.1da9aef5.chunk.js"
+  },
+  {
+    "revision": "38d6b9ccf3f75229c611",
+    "url": "/static/css/main.7af915c5.chunk.css"
+  },
+  {
+    "revision": "65cb1c6dc3d382fa2af12e564592b65e",
+    "url": "/index.html"
+  }
+];
