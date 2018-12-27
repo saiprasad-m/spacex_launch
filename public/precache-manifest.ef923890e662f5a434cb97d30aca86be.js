78542a2c96eead681735f15a2cabf050d717d10e
Sai Prasad <sai.prasad@rocketmail.com>
2018-12-27 23:29:18 +0530

    Added client folder built separately

---
+++ public/precache-manifest.ef923890e662f5a434cb97d30aca86be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bcc82c776ede97bf9c97",
+    "url": "/static/js/main.bcc82c77.chunk.js"
+  },
+  {
+    "revision": "47dcbb33ee32a097c989",
+    "url": "/static/js/1.47dcbb33.chunk.js"
+  },
+  {
+    "revision": "bcc82c776ede97bf9c97",
+    "url": "/static/css/main.7af915c5.chunk.css"
+  },
+  {
+    "revision": "d01f4783df73c96eb354bc0e75e2814f",
+    "url": "/index.html"
+  }
+];
