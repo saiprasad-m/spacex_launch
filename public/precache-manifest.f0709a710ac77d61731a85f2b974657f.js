30e43435ac5df1b892789314e052ab2e96c06581
Sai Prasad <sai.prasad@rocketmail.com>
2019-01-09 16:52:05 +0530

    Prod build rebuilt

---
+++ public/precache-manifest.f0709a710ac77d61731a85f2b974657f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0d282c3d76b3b159c778",
+    "url": "/static/js/main.0d282c3d.chunk.js"
+  },
+  {
+    "revision": "9d55cc1a09aa30569545",
+    "url": "/static/js/1.9d55cc1a.chunk.js"
+  },
+  {
+    "revision": "0d282c3d76b3b159c778",
+    "url": "/static/css/main.7af915c5.chunk.css"
+  },
+  {
+    "revision": "4a24daf117e434e88bc482f4bafb1a5e",
+    "url": "/index.html"
+  }
+];
